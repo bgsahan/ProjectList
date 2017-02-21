9fa3adb4fe912dcc0b68f541d89dafa021a7f9d6
bgsahan <bgsahan@yahoo.com>
2017-02-21 23:42:49 +0200

    Create CustomAdapter.java

---
+++ CustomAdapter.java
@@ -0,0 +1,48 @@
+public class CustomTodoAdapter extends ArrayAdapter<TodoProjekt> {
+
+    ArrayList<TodoProjekt> dataSet;
+    Context mContext;
+
+    public CustomTodoAdapter(Context context, ArrayList<TodoProjekt> data){
+        super(context, 0, data);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        final TodoProjekt todoProjekt = getItem(position);
+
+        if(convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.row_project_todo_layout, parent, false);
+        }
+
+        TextView name = (TextView) convertView.findViewById(R.id.textView1);
+        CheckBox cb = (CheckBox) convertView.findViewById(R.id.checkBox1);
+
+        name.setText(todoProjekt.getTodo());
+
+        if(todoProjekt.getChecked() == 1){
+            cb.setChecked(true);
+        }else{
+            cb.setChecked(false);
+        }
+
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(todoProjekt.getChecked() == 1){
+                    todoProjekt.setChecked(0);
+                    return;
+                }
+
+                if(todoProjekt.getChecked() == 0){
+                    todoProjekt.setChecked(1);
+                    return;
+                }
+            }
+        });
+
+        return convertView;
+    }
+}
